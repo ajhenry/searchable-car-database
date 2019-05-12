3c6b22ce1918ab8f35abee4f660a292243659090
Andrew Henry <ajh145@pitt.edu>
2019-05-12 13:25:18 -0400

    Updates

---
+++ precache-manifest.fb84a473bf99f5fcae81089d30a2a841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bead2c49b4449694a7d504595dec5bc4",
+    "url": "/searchable-car-database/index.html"
+  },
+  {
+    "revision": "56c4926ed23102b2c209",
+    "url": "/searchable-car-database/static/css/main.2d847972.chunk.css"
+  },
+  {
+    "revision": "f675030c617b90e90a4f",
+    "url": "/searchable-car-database/static/js/2.59f778a0.chunk.js"
+  },
+  {
+    "revision": "56c4926ed23102b2c209",
+    "url": "/searchable-car-database/static/js/main.c5cc7335.chunk.js"
+  },
+  {
+    "revision": "1b1d514fe77e2e4a03af",
+    "url": "/searchable-car-database/static/js/runtime~main.dde54317.js"
+  }
+]);
